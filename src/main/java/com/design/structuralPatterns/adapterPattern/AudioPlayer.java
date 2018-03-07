86217edc9e61e7e22f90d0ad21c20ff9a59f003d
杨智文 <yangzhiwen@chalco-steering.com>
2018-03-07 10:04:59 +0800

    修改注释

--- src/main/java/com/design/structuralPatterns/adapterPattern/AudioPlayer.java
+++ src/main/java/com/design/structuralPatterns/adapterPattern/AudioPlayer.java
@@ -9,6 +9,8 @@ public class AudioPlayer implements MediaPlayer {
 		if (audioType.equalsIgnoreCase("mp3")) {
 			System.out.println("mp3: " + fileName);
 		} 
+		// 适配器开始，上面是支持的格式，下面是不支持的格式
+		// 适配器相当于一个接口转换工具，把不支持的格式支持、适配
 		else if (audioType.equalsIgnoreCase("vlc") || audioType.equalsIgnoreCase("mp4")) {
 			mediaAdapter = new MediaAdapter(audioType);
 			mediaAdapter.play(audioType, fileName);
