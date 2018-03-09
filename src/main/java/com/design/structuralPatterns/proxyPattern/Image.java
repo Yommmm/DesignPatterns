290971d7d179ff3b0be1a327536d7909ea7b5dc0
杨智文 <yangzhiwen@chalco-steering.com>
2018-03-09 16:26:39 +0800

    proxy pattern

---
+++ src/main/java/com/design/structuralPatterns/proxyPattern/Image.java
@@ -0,0 +1,6 @@
+package com.design.structuralPatterns.proxyPattern;
+
+public interface Image {
+
+	void display();
+}
---
+++ src/main/java/com/design/structuralPatterns/proxyPattern/RealImage.java
@@ -0,0 +1,22 @@
+package com.design.structuralPatterns.proxyPattern;
+
+public class RealImage implements Image {
+
+	private String fileName;
+
+	
+	public RealImage(String fileName) {
+		this.fileName = fileName;
+		this.loadFromDisk(fileName);
+	}
+
+	@Override
+	public void display() {
+		System.out.println("Displaying: " + fileName);
+	}
+	
+	public void loadFromDisk(String fileName) {
+		System.out.println("Loading: " + fileName);
+	}
+
+}
